6db2d83250b314b1e7a9504d1bb2dc1fac59391a
zooeywm <zooeywm@gmail.com>
2023-12-13 17:10:32 +0800

    Fix(test): pause test

--- service/storage/tests/mod.rs
+++ service/storage/tests/mod.rs
@@ -6,37 +6,23 @@ use domain_storage::{
 use domain_workflow::mock::{MockNodeInstanceRepo, MockQueueRepo, MockWorkflowInstanceRepo};
 use service_storage::FileMoveServiceImpl;
 
+/// TODO
 #[tokio::test]
 async fn test_multipart_upload_from_net_disk() {
-    let shard_size = 512;
-    let test = include_bytes!("test_text");
+    // let shard_size = 512;
+    // let test = include_bytes!("test_text");
 
-    let file_name = "test_text";
-    let hash = blake3::hash(test).to_string();
-    let hahs_algorithm = HashAlgorithm::Blake3;
-    let size = 4001;
-    let count = 4001 / 512 + 1;
+    // let file_name = "test_text";
+    // let hash = blake3::hash(test).to_string();
+    // let hahs_algorithm = HashAlgorithm::Blake3;
+    // let size = 4001;
+    // let count = 4001 / 512 + 1;
 
-    // let parent_id = None;
-    // let meta_id = None;
-    let mut move_repo = MockMoveRegistrationRepo::new();
-    let mut snapshot_repo = MockSnapshotRepo::new();
-    let mut node_instance_repo = MockNodeInstanceRepo::new();
-    let mut workflow_instance_repo = MockWorkflowInstanceRepo::new();
-    let mut queue_repo = MockQueueRepo::new();
-
-    // // let move_service = FileMoveServiceImpl::builder()
-    // let repos = MockRepos::new().expect_get_by_id().return_once(|_| {
-    //     Ok(MoveRegistration {
-    //         id: todo!(),
-    //         meta_id: todo!(),
-    //         file_name: todo!(),
-    //         hash,
-    //         hash_algorithm: HashAlgorithm::Blake3,
-    //         size: todo!(),
-    //         destination: todo!(),
-    //         is_upload_failed: todo!(),
-    //         failed_reason: todo!(),
-    //     })
-    // });
+    // // let parent_id = None;
+    // // let meta_id = None;
+    // let mut move_repo = MockMoveRegistrationRepo::new();
+    // let mut snapshot_repo = MockSnapshotRepo::new();
+    // let mut node_instance_repo = MockNodeInstanceRepo::new();
+    // let mut workflow_instance_repo = MockWorkflowInstanceRepo::new();
+    // let mut queue_repo = MockQueueRepo::new();
 }
