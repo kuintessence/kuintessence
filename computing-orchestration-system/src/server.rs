d87e038ce98f417713d4899db51c7ac10614f067
zooeywm <zooeywm@gmail.com>
2024-03-20 13:55:52 +0800

    clippy code

--- computing-orchestration-system/src/server.rs
+++ computing-orchestration-system/src/server.rs
@@ -42,8 +42,7 @@ pub async fn async_run() {
         }
     };
     let common_config: alice_infrastructure::config::CommonConfig = service_provider.provide();
-    if let Err(e) = alice_infrastructure::telemetry::init_telemetry(&common_config.telemetry)
-    {
+    if let Err(e) = alice_infrastructure::telemetry::init_telemetry(&common_config.telemetry) {
         return eprintln!("{}: {}", "Cannot build logger".red(), e);
     };
     let tasks: Vec<Arc<dyn BackgroundService>> = service_provider.provide();
@@ -76,7 +75,7 @@ pub async fn initialize_web_host(sp: Arc<ServiceProvider>) {
     let rc = common_config.host.resources_config.clone();
     let database: Arc<Database> = sp.provide();
 
-    match actix_web::HttpServer::new(move || {
+    let service_factory = move || {
         let resources = I18NResources::builder()
             .add_path("resources")
             .use_isolating(false)
@@ -160,15 +159,17 @@ pub async fn initialize_web_host(sp: Arc<ServiceProvider>) {
                 StatusCode::BAD_REQUEST,
                 middleware::error_msg_i18n::add_error_header,
             ))
-    })
-    .bind((
-        common_config.host.bind_address.to_owned(),
-        common_config.host.bind_port,
-    ))
-    .unwrap()
-    .disable_signals()
-    .run()
-    .await
+    };
+
+    match actix_web::HttpServer::new(service_factory)
+        .bind((
+            common_config.host.bind_address.to_owned(),
+            common_config.host.bind_port,
+        ))
+        .unwrap()
+        .disable_signals()
+        .run()
+        .await
     {
         Ok(_) => info!("Web server stopped successfully."),
         Err(e) => error!("Web server into erorr: {}", e),
